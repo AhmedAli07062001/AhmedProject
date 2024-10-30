42a0c8b675539536528debf29901ab2fd80da5d2
Ahmed Ali <ahmedali29090067@gamil.com>
2024-10-30 11:07:41 +0530

    add files

--- website/__init__.py
+++ website/__init__.py
@@ -6,12 +6,28 @@ from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'dfkjdkfd fjkdfdjkfjdkd fddfdd'
+    app.config['SECRET_KEY'] = 'dont try to cheat my secret key its impossible'
 
 
     # Route for the home page
     @app.route('/')
     def home():
         return render_template('index.html')  # Render the home page
+    
+    @app.route('/login')
+    def login():
+        return render_template('login.html')   #render to the login page
+    
+    @app.route('/sign-up')
+    def sign_up():
+        return render_template('signup.html')  #render to the sign-up page
+    
+    @app.route('/OTP-Verification')
+    def opt_verification():
+        return render_template('OTP Verification.html')   #render to the opt verification page
+
+    @app.route('/New-Password')
+    def new_password():
+        return render_template('new password.html')   
 
     return app
