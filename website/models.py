bf6778ada2cecae48b127e4566ba9b6ea64a157b
Ahmed Ali <ahmedali29090067@gamil.com>
2024-11-06 11:29:02 +0530

    add backend logic

--- website/models.py
+++ website/models.py
@@ -1,27 +1,29 @@
-# models.py
-
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from werkzeug.security import check_password_hash
 
-db = SQLAlchemy()
+# Import db object
+from . import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-
-    user_id = db.Column(db.Integer, primary_key=True)
+class User(UserMixin, db.Model):
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
     phone_number = db.Column(db.String(10), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    role = db.Column(db.String(50), nullable=False, 
-                     check_constraint="role IN ('Patient', 'Therapist', 'Supervisor', 'Admin')")
-    profile_pic = db.Column(db.String(255), nullable=True)
-    date_registered = db.Column(db.Date, nullable=False, default=datetime.utcnow)
-    last_login = db.Column(db.Date, nullable=True)
+    address = db.Column(db.String(255), nullable=True)
+    role = db.Column(db.String(50), nullable=False,default='Patient')
+    profile_pic = db.Column(db.String(255))
+    date_registered = db.Column(db.Date, nullable=False,default=db.func.current_date())
+    last_login = db.Column(db.Date)
     password_hash = db.Column(db.String(255), nullable=False)
     gender = db.Column(db.String(10), nullable=False)
-    last_updated = db.Column(db.TIMESTAMP, nullable=False, default=datetime.utcnow, onupdate=datetime.utcnow)
+    last_updated = db.Column(db.TIMESTAMP, server_default=func.now(), onupdate=func.now())
 
-    def __repr__(self):
-        return f'<User {self.email}>'
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_id(self):
+        return self.user_id
